d82b08035d4a624a51dd600e70861441b794ec14
Rohitkumar Pandit <rohit54pandit@gmail.com>
2023-12-20 21:01:20 +0530

    fixed

--- app/components/Story.js
+++ app/components/Story.js
@@ -8,14 +8,21 @@ import 'swiper/css';
 function Story() {
   return (
     <>
-                <div className='home-intro'>
-                <div className="home-intro-header">
-                    <h2 className="home-intro-heading">Everybody has a story</h2>
-                    <div className="home-intro-heading-outline">Everybody has a story</div>
+                <div className='pb-6 md:pb-8 mt-12 overflow-hidden'>
+            <div className='md:mx-auto px-4 lg:max-w-[90rem] md:max-w-[48rem]'>
+                <div className="text-center relative">
+                    <h2 
+                    style={{fontFamily:"Gotham,sans-serif"}}
+                    className="font-bold -z-1 text-white uppercase relative text-3xl md:text-5xl lg:text-[4.7rem] leading-none"
+                    >Everybody has a story</h2>
+                    <div 
+                     style={{fontFamily:"Gotham,sans-serif"}}
+                    className=" font-bold home-intro-heading-outline z-10 stroke-white text-fill-transparent text-white uppercase text-3xl md:text-5xl lg:text-[4.7rem] leading-none absolute left-0 right-0 top-0 bottom-auto">
+                        Everybody has a story</div>
                 </div>
-                <div className='home-intro-row'>
-                    <div className='home-intro-media'>
-                        <div className='slider-rows home-slider-rows'>
+                <div className='md:block lg:flex lg:items-stretch'>
+                    <div className='lg:flex-none md:w-auto lg:w-1/2'>
+                        <div className='lg:-mt-10 md:-mt-6 -mt-4 -mx-4 md:mx-4'>
                           
 
                             <Swiper
@@ -164,17 +171,17 @@ function Story() {
                         </div>
 
                     </div>
-                    <div className="home-intro-text">
+                    <div className="home-intro-texts flex flex-col justify-between relative lg:pt-8 pt-10 text-center md:pl-32 md:pr-20">
                         <div className="home-intro-text-top">
                             <img
                                 src="https://assets-global.website-files.com/6329724b2767486ad124b49b/639555752721b31a1a726680_round-text.png"
                                 loading="lazy"
                                 data-w-id="b8b6ec2e-d963-e37e-a850-5bd0c9a2d1b9"
                                 alt=""
-                                className="home-intro-text-image"
+                                className="home-intro-text-image hidden first-lettermd:w-36 md:h-36 md:top-auto md:block md:ml-auto md:-bottom-5 md:left-auto md:-right-[10%] absolute lg:-bottom-1/3 lg:-left-[16%]"
 
                             />
-                            <p className="paragraph">
+                            <p className="paragraph font-medium">
                                 In the new climate of digital media, being unique and captivating is the only way to stand out.
                                 <br />
                                 <br />
@@ -193,7 +200,7 @@ function Story() {
                         </div>
                     </div>
                 </div>
-
+                </div>
             </div>
     </>
   )
